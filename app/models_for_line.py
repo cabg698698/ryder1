88d0710f0ec0153a173e956d8407c51c43f5a098
cabg698698 <96216074+cabg698698@users.noreply.github.com>
2024-06-06 11:29:32 +0800

    Add files via upload

--- app/models_for_line.py
+++ app/models_for_line.py
@@ -14,13 +14,15 @@ def reply_text(event):
             print(event.message.text)
             if event.message.text == "查詢指令說明":
                 reply = PhoebeTalks.search_explain(event)#查詢指令說明
+            elif event.message.text == "修改指令說明":
+                reply = PhoebeTalks.update_explain(event)#查詢指令說明
             elif "查:" in event.message.text:
                 reply = PhoebeTalks.search_record(event)#查詢資料
             elif ("修改:" in event.message.text) or ("新增:" in event.message.text):
                 reply = PhoebeTalks.update_record(event)#修改或新增資料
-            elif "增加資料:" in event.message.text:
+            elif "新增地點:" in event.message.text:
                 reply = PhoebeTalks.insert_record(event)#增加資料
-            elif "刪除資料" in event.message.text:
+            elif "刪除地點:" in event.message.text:
                 reply = PhoebeTalks.delete_record(event)#刪除資料
 
             else:
